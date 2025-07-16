899e6cb21c6559b4d6ec8a321b1ed4d51a130674
Caio-Calhau <caiocalhaum@gmail.com>
2025-07-16 19:27:20 -0300

    update: change a few plugins

--- nvim/.config/nvim/lua/caio/core/keymaps.lua
+++ nvim/.config/nvim/lua/caio/core/keymaps.lua
@@ -11,11 +11,11 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "<C-c>", "<ESC>", { desc = "Exit insert mode with C-c" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })                            -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })                            -- decrement
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 keymap.set("n", "<Leader>w", ":w<CR>", { desc = "Save file" })
 keymap.set("n", "<Leader>ee", "<cmd>Explore<CR>", { desc = "Open explorer" })
@@ -32,9 +32,9 @@ keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 
 keymap.set("n", "<leader>f", function()
-  require("conform").format({ bufnr = 0 })
+	require("conform").format({ bufnr = 0 })
 end)
 
 keymap.set("n", "<leader>ca", function()
-  require("cellular-automaton").start_animation("make_it_rain")
+	require("cellular-automaton").start_animation("make_it_rain")
 end)
--- nvim/.config/nvim/lua/caio/plugins/init.lua
+++ nvim/.config/nvim/lua/caio/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "nvim-lua/plenary.nvim",
-    "christoomey/vim-tmux-navigator",
-  },
-  "eandrju/cellular-automaton.nvim",
+	{
+		"nvim-lua/plenary.nvim",
+		"christoomey/vim-tmux-navigator",
+		"eandrju/cellular-automaton.nvim",
+	},
 }
