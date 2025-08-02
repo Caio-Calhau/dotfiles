9c6900913f1d093fcc1925f06e4861cf1f0f2f5c
caio <calhau.caio@smartlybrasil.com.br>
2025-08-02 12:50:14 -0300

    refactor: entire project for semantics

--- nvim/.config/nvim/lua/golira/lazy.lua
+++ nvim/.config/nvim/lua/golira/lazy.lua
@@ -11,7 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	spec = "golira.lazy",
-	change_detection = { notify = false },
+require("lazy").setup({ { import = "golira.plugins" } }, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
--- nvim/.config/nvim/lua/golira/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/golira/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
 	config = function()
 		local null_ls = require("null-ls")
@@ -7,6 +10,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.completion.spell,
+				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 				-- null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.black,
