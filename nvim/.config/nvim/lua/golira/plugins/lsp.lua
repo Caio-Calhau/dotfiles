24df4684400ea292da385d7ac510968d983482c5
Caio-Calhau <caiocalhaum@gmail.com>
2025-08-02 21:02:30 -0300

    refactor: lsp to work with completions

--- nvim/.config/nvim/lua/golira/plugins/lsp.lua
+++ nvim/.config/nvim/lua/golira/plugins/lsp.lua
@@ -1,151 +1,58 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"stevearc/conform.nvim",
+	{
 		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"j-hui/fidget.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"stevanmilic/nvim-lspimport",
-		"nvimtools/none-ls.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
 	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	run = "make install_jsregexp",
-
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {},
-		})
-		-- local cmp = require("cmp")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		vim.lsp.enable({
-			"luals",
-			"pyright",
-			"emmet_ls",
-			"jsonls",
-			"tsserver",
-			"cssls",
-			"html",
-			"yamlls",
-			"tailwindcss",
-			"svelte",
-		})
-
-		vim.diagnostic.config({
-			update_in_insert = true,
-			virtual_text = true,
-			float = {
-				focusable = false,
-				style = "minimal",
-				border = "rounded",
-				source = "always",
-				header = "",
-				prefix = "",
-			},
-		})
-
-		vim.lsp.enable("eslint", {
-			cmd = { "eslint_d", "vscode-eslint-language-server", "--stdio" },
-			filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-		})
-
-		require("lspconfig").eslint.setup({
-			settings = {
-				workingDirectories = {
-					mode = "auto",
+			vim.diagnostic.config({
+				update_in_insert = true,
+				virtual_text = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
 				},
-			},
-		})
+			})
 
-		require("fidget").setup({})
+			local lspconfig = require("lspconfig")
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"stylua",
-				"luacheck",
-				"shellcheck",
-				"shfmt",
-				"prettierd",
-				"prettier",
-				"eslint_d",
-				"eslint",
-				"jsonlint",
-				"yamllint",
-				"markdownlint",
-			},
-		})
-
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"emmet_ls",
-				"pyright",
-			},
-
-			handlers = {
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								format = {
-									enable = true,
-									-- Put format options here
-									-- NOTE: the value should be STRING!!
-									defaultConfig = {
-										indent_style = "space",
-										indent_size = "2",
-									},
-								},
-							},
-						},
-					})
-				end,
-
-				["emmet_ls"] = function()
-					local lspconfig = require("lspconfig")
-					-- configure emmet language server
-					lspconfig["emmet_ls"].setup({
-						capabilities = capabilities,
-						filetypes = {
-							"html",
-							"typescriptreact",
-							"javascriptreact",
-							"javascript",
-							"typescript",
-							"css",
-							"sass",
-							"scss",
-							"less",
-							"svelte",
-						},
-					})
-				end,
-			},
-		})
-	end,
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
