340c473f576bfbcf8915d5a2faf0787339ec7f08
caiocalhau <caiocalhaum@gmail.com>
2026-01-23 22:40:37 -0300

    feat: set vsplit on diff

--- nvim/.config/nvim/lua/golira/plugins/neogit.lua
+++ nvim/.config/nvim/lua/golira/plugins/neogit.lua
@@ -6,8 +6,6 @@ return {
 		"sindrets/diffview.nvim", -- optional - Diff integration
 
 		-- Only one of these is needed.
-		"nvim-telescope/telescope.nvim", -- optional
-		"ibhagwan/fzf-lua", -- optional
 		"nvim-mini/mini.pick", -- optional
 		"folke/snacks.nvim", -- optional
 	},
@@ -15,4 +13,5 @@ return {
 	keys = {
 		{ "<leader>gs", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
 	},
+	kind = { "vsplit" },
 }
