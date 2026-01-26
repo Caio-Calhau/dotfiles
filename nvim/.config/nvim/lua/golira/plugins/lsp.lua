05b349d2b2847e17e04e0f69d3d4a07437d6af00
caiocalhau <caiocalhaum@gmail.com>
2026-01-26 20:39:55 -0300

    remove super-maven

--- nvim/.config/nvim/lua/golira/plugins/lsp.lua
+++ nvim/.config/nvim/lua/golira/plugins/lsp.lua
@@ -14,18 +14,6 @@ return {
 		},
 	},
 	{
-		"supermaven-inc/supermaven-nvim",
-		config = function()
-			require("supermaven-nvim").setup({
-				keymaps = {
-					accept_suggestion = "<Tab>",
-					clear_suggestion = "<C-]>",
-					accept_word = "<C-j>",
-				},
-			})
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
