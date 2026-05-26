4457176ad645acf4276a1eb8fb26d5b95a04f828
caio <caiocalhaum@gmail.com>
2026-05-26 16:46:41 -0300

    update: telescope styles

--- nvim/.config/nvim/lua/golira/plugins/telescope.lua
+++ nvim/.config/nvim/lua/golira/plugins/telescope.lua
@@ -1,30 +1,44 @@
 return {
 	"nvim-telescope/telescope.nvim",
 
-	tag = "0.1.5",
+	branch = "0.1.x",
 
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 
 	config = function()
-		require("telescope").setup({})
-
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_strategy = "horizontal",
+			},
+		})
+
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+
 		vim.keymap.set({ "n", "v" }, "<leader>rg", builtin.live_grep, { noremap = true, silent = true })
+
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
 		end)
+
 		vim.keymap.set("n", "<leader>pWs", function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
+
 		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			builtin.grep_string({
+				search = vim.fn.input("Grep > "),
+			})
 		end)
+
 		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 	end,
 }
