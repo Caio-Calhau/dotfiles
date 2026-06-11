b34c575da4b1144086e42316b79e5d899ab37b01
caio <caiocalhaum@gmail.com>
2026-06-11 08:41:21 -0300

    feat: open fugitive at side panel and implement toggle functionality

--- nvim/.config/nvim/lua/golira/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/golira/plugins/fugitive.lua
@@ -1,7 +1,18 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		local function toggle_fugitive()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				local buf = vim.api.nvim_win_get_buf(win)
+				if vim.bo[buf].ft == "fugitive" then
+					vim.api.nvim_win_close(win, true)
+					return
+				end
+			end
+			vim.cmd("vertical Git | vertical resize 40")
+		end
+
+		vim.keymap.set("n", "<leader>gs", toggle_fugitive)
 
 		local Golira_Fugitive = vim.api.nvim_create_augroup("Golira_Fugitive", {})
 
